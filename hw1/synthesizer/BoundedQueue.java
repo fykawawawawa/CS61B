40642c065f2dba8517bf58d0e96314d5b0be1316
fykawawawawa <455653863@qq.com>
2022-09-23 20:49:23 +0800

    hw1 first commit

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,26 @@
+package synthesizer;
+import java.util.Iterator;
+
+public interface BoundedQueue<T> extends Iterable<T>{
+    int capacity();
+    int fillCount();
+    void enqueue(T x);
+    T dequeue();
+    T peek();
+    default boolean isEmpty(){
+        if(fillCount()==0)
+            return true;
+        else
+            return false;
+    }
+    default boolean isFull(){
+        if(fillCount()==capacity())
+            return true;
+        else
+            return false;
+    }
+
+
+    @Override
+    Iterator<T> iterator();
+}
